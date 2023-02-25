a51ed46edabca0ddb0b026f3ebd6e40f53b84bcf
inganta23 <johanes230500@gmail.com>
2023-02-25 11:07:09 +0700

    feat: add todo

---
+++ pages/todo.jsx
@@ -0,0 +1,77 @@
+import Image from "next/image";
+import React from "react";
+import todo from "../public/assets/todo.png";
+import { RiRadioButtonFill } from "react-icons/ri";
+import Link from "next/link";
+
+const portfolio = () => {
+  return (
+    <div className="w-full">
+      <div className="w-screen h-[30vh] lg:h-[40vh] relative">
+        <div className="absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10"></div>
+        <Image
+          className="absolute z-1"
+          layout="fill"
+          objectFit="cover"
+          src={todo}
+          alt="/"
+        />
+        <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
+          <h2 className="py-2">Todo - List Website</h2>
+          <h3>React JS / Express JS / Prisma / PostgreSQL </h3>
+        </div>
+      </div>
+
+      <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 pt-8">
+        <div className="col-span-4">
+          <h2>Project Overview</h2>
+          <p>
+            This my todo list website. In this website you can register, login
+            and create your own todo list. I deploy this website using railway
+          </p>
+          <a
+            href="https://simple-rest-api-production.up.railway.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="px-8 py-2 mt-4 mr-8">Demo</button>
+          </a>
+          <a
+            href="https://github.com/inganta23/simple-rest-api"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="px-8 py-2 mt-4">Code</button>
+          </a>
+        </div>
+        <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4">
+          <div className="p-2">
+            <p className="text-center font-bold pb-2">Technologies</p>
+            <div className="grid grid-cols-3 md:grid-cols-1">
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> React JS
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Tailwind
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Prisma
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Express JS
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> PostgreSQL
+              </p>
+            </div>
+          </div>
+        </div>
+        <Link href="/#projects">
+          <p className="underline cursor-pointer">Back</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default portfolio;
